c67e4bedc12dacc2915e0f9b3a94cbb9b30bbb83
Viachaslau Maslau <v.maslau@arateg.com>
2021-12-05 14:42:38 +0300

    add: subtask 2

---
+++ subtask_02.js
@@ -0,0 +1,9 @@
+const users = [{ name: 'Ivan', age: 18 }, { name: 'Petr', age: 12 }, { name: 'Sidor', age: 25 }, { name: 'Pavel', age: 16 }, { name: 'Sasha', age: 29 }]
+
+let sumYoung = 0
+
+for (let i = 0; i< users.length; i++) {
+  if (users[i].age < 18) {
+    sumYoung += users[i].age
+  }
+}
