16e3c4935a483ce1674272bb2cf36823cb32dc9a
Viachaslau Maslau <v.maslau@arateg.com>
2021-12-12 19:40:39 +0300

    fix console log of subtask 1

--- subtask_1.js
+++ subtask_1.js
@@ -3,4 +3,4 @@ const users = [{ name: 'Ivan', age: 18 }, { name: 'Petr', age: 12 }, { name: 'Si
 const sortedUsers = users.sort((a, b) => a.age > b.age ? 1 : -1)
 const sumYoung = users.reduce((sum, user) => user.age < 18 ? sum + user.age : sum, 0)
 
-console.log(`Sorted by age: ${JSON.stringify(sortedUsers)}\nSum: ${sumYoung}`)
+console.log(sortedUsers, sumYoung)
