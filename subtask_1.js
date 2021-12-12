4a9ad43b8aec6091a6bdc73617282e7335ae26d3
Viachaslau Maslau <v.maslau@arateg.com>
2021-12-12 19:07:53 +0300

    add: some functions

--- subtask_1.js
+++ subtask_1.js
@@ -1,12 +1,31 @@
-function avg () {
-  const numbers = [...arguments]
+const avgArrow = () => {
   let sum = 0
 
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i]
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
   }
 
-  return sum / numbers.length
+  return sum / arguments.length
+}
+
+const avgExpression = function () {
+  let sum = 0
+
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+
+  return sum / arguments.length
+}
+
+function avg () {
+  let sum = 0
+
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+
+  return sum / arguments.length
 }
 
 const example = avg(1, 2, 3, 4, 5, 6, 10, 20, 30)
